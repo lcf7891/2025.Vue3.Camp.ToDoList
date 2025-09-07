03de4666bf885e66f91ac02e7fdefdbeee2b931f
Allen <lcf7891@gmail.com>
2025-09-08 03:40:01 +0800

    feat: 移除 router 驗證機制

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,4 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import { getToken } from '@/composables/useCookie'
-import { apiRequest } from '@/composables/useApi'
-import { useToastStore } from '@/stores/useToastStore'
 
 const routes = [
   {
@@ -32,8 +29,7 @@ const routes = [
     name: 'TodoList',
     component: () => import('../views/TodoListView.vue'),
     meta: {
-      title: '待辦事項清單',
-      requiresAuth: true
+      title: '待辦事項清單'
     }
   },
   {
@@ -50,40 +46,9 @@ const router = createRouter({
   routes
 })
 
-// router.beforeEach((to, form, next) => {
-//   document.title = to.meta.title || '我的待辦清單'
-//   next()
-// })
-router.beforeEach(async (to) => {
+router.beforeEach((to, form, next) => {
   document.title = to.meta.title || '我的待辦清單'
-  const toast = useToastStore()
-  const token = getToken()
-
-  // 未登入 → 禁止進入受保護頁面
-  if (to.meta.requiresAuth && !token) {
-    return { name: 'Login' }
-  }
-
-  // 已登入 → 禁止進入登入頁
-  if (token && to.name === 'Login') {
-    return { name: 'TodoList' }
-  }
-
-  // 若需要驗證 token 是否有效
-  if (to.meta.requiresAuth && token) {
-    try {
-      const result = await apiRequest('users/checkout', 'GET')
-      if (!result.status) {
-        toast.showToast('Token 驗證失敗', '請重新登入')
-        return { name: 'Login' }
-      }
-    } catch (error) {
-      toast.showToast('Token 驗證失敗', error.message)
-      return { name: 'Login' }
-    }
-  }
-
-  return true
+  next()
 })
 
 export default router
