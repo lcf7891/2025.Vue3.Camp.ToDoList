121b179bf8440d4fa5c45f5e1b5fb752c54bc776
lcf7891 <53992879+lcf7891@users.noreply.github.com>
2025-09-08 09:24:58 +0800

    feat: 修改 LayoutAuth.vue 路徑

--- src/components/LayoutAuth.vue
+++ src/components/LayoutAuth.vue
@@ -5,10 +5,10 @@
     <div class="w-full lg:w-[386px]">
       <img
         class="mx-auto mb-4 w-full lg:w-[85%]"
-        src="/src/assets/images/authlogo.png"
+        src="@/assets/images/authlogo.png"
         alt="todolist logo"
       />
-      <img class="hidden lg:block" src="/src/assets/images/authleft.png" alt="登入與註冊插圖" />
+      <img class="hidden lg:block" src="@/assets/images/authleft.png" alt="登入與註冊插圖" />
     </div>
     <div class="ml-0 w-full lg:ml-26 lg:w-[304px]">
       <slot />
