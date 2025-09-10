a9994818b604e37a83e6de33d3928109631b21e9
Allen <lcf7891@gmail.com>
2025-09-11 00:20:30 +0800

    feat: 停用 useApi.js 攔截器

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
-import { getToken } from './useCookie'
+// import { getToken } from './useCookie'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
 })
-api.interceptors.request.use((config) => {
-  const token = getToken()
-
-  if (token) {
-    config.headers.Authorization = token
-  }
-  return config
-})
+// api.interceptors.request.use((config) => {
+//   const token = getToken()
+//   console.log('getToken', token)
+//   if (token) {
+//     config.headers.Authorization = token
+//   }
+//   return config
+// })
 export const apiRequest = async (url, method, data) => {
   try {
     const config = {
