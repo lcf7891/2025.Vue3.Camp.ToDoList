c6ae1e762420c7c6598a8e43c638b1ce973e2758
Allen <lcf7891@gmail.com>
2025-09-11 00:56:54 +0800

    chore: 刪除 useApi 的 console

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -4,14 +4,15 @@ import { getToken } from './useCookie'
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
 })
+
 api.interceptors.request.use((config) => {
   const token = getToken()
-  console.log('getToken', token)
   if (token) {
     config.headers.Authorization = token
   }
   return config
 })
+
 export const apiRequest = async (url, method, data) => {
   try {
     const config = {
