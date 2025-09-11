e22b82df0cf26bd402a5c392cac7074fd386c1c5
Allen <lcf7891@gmail.com>
2025-09-11 18:09:13 +0800

    style: 增加 .prettierrc.json 設定並 format

--- src/components/LayoutAuth.vue
+++ src/components/LayoutAuth.vue
@@ -3,11 +3,7 @@
     class="container mx-auto flex h-screen flex-col items-center px-8 pt-12 lg:flex-row lg:justify-center lg:p-0"
   >
     <div class="w-full lg:w-[386px]">
-      <img
-        class="mx-auto mb-4 w-full lg:w-[85%]"
-        :src="authLogo"
-        alt="todolist logo"
-      />
+      <img class="mx-auto mb-4 w-full lg:w-[85%]" :src="authLogo" alt="todolist logo" />
       <img class="hidden lg:block" :src="authLeft" alt="登入與註冊插圖" />
     </div>
     <div class="ml-0 w-full lg:ml-26 lg:w-[304px]">
@@ -16,6 +12,6 @@
   </div>
 </template>
 <script setup>
-import authLogo from '@/assets/images/authlogo.png'
-import authLeft from '@/assets/images/authleft.png'
+  import authLogo from '@/assets/images/authlogo.png'
+  import authLeft from '@/assets/images/authleft.png'
 </script>
