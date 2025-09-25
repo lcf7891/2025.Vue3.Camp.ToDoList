0d1e9283c43abb2ceb2b89fd313dee8bf2bbfe9e
Allen <lcf7891@gmail.com>
2025-09-25 21:51:53 +0800

    style: 隱藏過長的資訊

--- src/components/ToastMessage.vue
+++ src/components/ToastMessage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="fixed top-6 right-6 z-50" v-if="toasts.length">
     <div
-      class="bg-table/80 mb-4 w-[150px] rounded-md text-sm shadow-lg backdrop-blur-md lg:w-[300px]"
+      class="bg-table/80 mb-4 w-[150px] truncate rounded-md text-sm shadow-lg backdrop-blur-md lg:w-[300px]"
       v-for="toast in toasts"
       :key="toast.id"
     >
       <h5 class="border-b-muted text-info border-b p-2">{{ toast.title }}</h5>
-      <p class="truncate p-2">{{ toast.content }}</p>
+      <p class="p-2">{{ toast.content }}</p>
     </div>
   </div>
 </template>
